9cd4496d0d1dc5f4b936cef6efbbef5512f69d98
Feargus O'Gorman <feargusog@gmail.com>
2026-03-09 15:14:20 +0000

    remove unused flag in scalar regression head

--- src/cellkit/model/heads.py
+++ src/cellkit/model/heads.py
@@ -3,13 +3,11 @@ import torch
 class ScalarRegressionHead(torch.nn.Module):
     def __init__(
         self,
-        d_model: int,
-        use_batch_labels: bool = False,
+        d_model: int
     ):
         super().__init__()
-        d_in = d_model * 2 if use_batch_labels else d_model
         self.fc = torch.nn.Sequential(
-            torch.nn.Linear(d_in, d_model),
+            torch.nn.Linear(d_model, d_model),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(d_model, d_model),
             torch.nn.LeakyReLU(),
